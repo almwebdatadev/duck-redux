48fe458a9245dd1893f3622b23c8e3204c152e99
Training222admin <Training222admin>
2019-06-14 10:44:32 +0200

    refactor_react_redux_done

--- src/components/Freezer/FreezerContainer.js
+++ src/components/Freezer/FreezerContainer.js
@@ -3,9 +3,7 @@ import store from "../../store";
 import { actions as actionsFreezer } from "../../ducks/freezer";
 import * as FLAVORS from "../../constants/icecreams_flavors";
 import Freezer from "../Freezer/Freezer";
-// import Panel from '../Panel/Panel';
-// import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
-// import Button from "../Button/Button";
+import { connect } from 'react-redux';
 
 
 class FreezerContainer extends Component {
@@ -14,27 +12,30 @@ class FreezerContainer extends Component {
     temperature: store.getState().freezer.temperature
   }
   componentDidMount() {
-    this.unsuscribe = store.subscribe(() =>
-      this.setState({
-        flavors: store.getState().freezer.icecreams,
-        temperature: store.getState().freezer.temperature
-      }))
+    // this.unsuscribe = store.subscribe(() =>
+    //   this.setState({
+    //     flavors: store.getState().freezer.icecreams,
+    //     temperature: store.getState().freezer.temperature
+    //   }))
     setInterval(() => {
       const randomTemp = -Math.round(Math.random() * 10)
-      store.dispatch(actionsFreezer.updateTemperature(randomTemp))
+      // store.dispatch(actionsFreezer.updateTemperature(randomTemp))
+      this.props.updateTemperature(randomTemp)
     }, 2000)
   }
 
 
   //en este caso no seria necesario por que siempre esta este comopnente en pantalla
   componentWillUnmount() {
-    this.unsuscribe()
+    // this.unsuscribe()
   }
 
   handleRestock = (flavorName) => {
-    const amount = parseInt(window.prompt(`Enter number of scoops to restock ${flavorName}`))
+    const amount =
+      parseInt(window.prompt(`Enter number of scoops to restock ${flavorName}`))
     if (!isNaN(amount)) {
-      store.dispatch(actionsFreezer.addIcecream(flavorName, amount))
+      // store.dispatch(actionsFreezer.addIcecream(flavorName, amount))
+      this.props.addIcecream(flavorName, amount)
     }
   }
 
@@ -48,24 +49,12 @@ class FreezerContainer extends Component {
   }
 
   render() {
-    // console.log(this.state);
-    // const flavors = this.state.flavors;
-    // const freezerFlavors = Object.keys(flavors).map(flavorName =>
-    //   <FreezerFlavor
-    //     key={flavorName}
-    //     flavorName={flavorName}
-    //     onClickRestock={() => this.handleRestock(flavorName)}
-    //     scoops={flavors[flavorName]} />
-    // )
+
     return (
-      // <Panel title={`Freezer (º${this.state.temperature}C)`}>
-      //   <Button label='Add flavor' onClick={this.handleClickAddFlavor} />
-      //   <br />
-      //   {freezerFlavors}
-      // </Panel>
+
       <Freezer
-        flavors={this.state.flavors}
-        temperature={this.state.temperature}
+        flavors={this.props.flavors}
+        temperature={this.props.temperature}
         onClickAddFlavor={this.handleClickAddFlavor}
         onClickRestock={this.handleRestock}
       />
@@ -74,5 +63,19 @@ class FreezerContainer extends Component {
   }
 }
 
-export default FreezerContainer;
+///cambiar?¿ hay que meternlo dentro de la clase?
+const mapStateToProps = state => ({
+  flavors: state.freezer.icecreams,
+  temperature: state.freezer.temperature
+})
+
+
+const mapDispatchToProps = dispatch => ({
+  updateTemperature:
+    temperature => dispatch(actionsFreezer.updateTemperature(temperature)),
+  addIcecream:
+    (flavorName, amount) => dispatch(actionsFreezer.addIcecream(flavorName, amount)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(FreezerContainer);
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import store from "./store";
 import * as flavors from "./constants/icecreams_flavors";
 import { actions as actionsFreezer } from "./ducks/freezer";
-//import {actions as actionsOrders} from "./ducks/orders";
+import { Provider } from 'react-redux'
+
 
 setInterval(() =>
     store.dispatch(actionsFreezer.addIcecream(flavors.FABADA, 5)
         , 1500)
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    ), document.getElementById('root')
+);
 console.log(store.getState());
 
+
+///// cambiar?¿
 store.subscribe(() => console.log(store.getState()));
 store.dispatch(actionsFreezer.updateTemperature(-9));
-// store.dispatch(actions.addIcecream(flavors.FABADA, 33));
 
