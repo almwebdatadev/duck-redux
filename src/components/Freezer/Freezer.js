0461f4860f56cca57aa045e440a10b1c36ef64e8
Training222admin <Training222admin>
2019-06-14 09:49:08 +0200

    refactor_v1_container_and_presentational

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,72 +1,73 @@
 import React, { Component } from 'react';
-
 import Panel from '../Panel/Panel';
 import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
-import { actions as actionsFreezer } from "../../ducks/freezer";
-import store from "../../store";
-import * as FLAVORS from "../../constants/icecreams_flavors";
 import Button from "../Button/Button";
 
-
 class Freezer extends Component {
-  state = {
-    flavors: store.getState().freezer.icecreams,
-    temperature: store.getState().freezer.temperature
-  }
-  componentDidMount() {
-    this.unsuscribe = store.subscribe(() =>
-      this.setState({
-        flavors: store.getState().freezer.icecreams,
-        temperature: store.getState().freezer.temperature
-      }))
-    setInterval(() => {
-      const randomTemp = -Math.round(Math.random() * 10)
-      store.dispatch(actionsFreezer.updateTemperature(randomTemp))
-    }, 2000)
-  }
-
-
-  //en este caso no seria necesario por que siempre esta este comopnente en pantalla
-  componentWillUnmount() {
-    this.unsuscribe()
-  }
-
-  handleRestock = (flavorName) => {
-    const amount = parseInt(window.prompt(`Enter number of scoops to restock ${flavorName}`))
-    if (!isNaN(amount)){
-      store.dispatch(actionsFreezer.addIcecream(flavorName, amount))
-    }
-  }
-
-  handleClickAddFlavor = () => {
-    const availableFlavors=Object.keys(FLAVORS)
-    const flavorToRestock =
-      window.prompt(`Enter flavor to restock from ${availableFlavors.join(',')}`)
-      if (FLAVORS[flavorToRestock]){
-        this.handleRestock(flavorToRestock)
-      }
-  }
 
   render() {
-    console.log(this.state);
-    const flavors = this.state.flavors;
+    console.log(this.props);
+    const flavors = this.props.flavors;
     const freezerFlavors = Object.keys(flavors).map(flavorName =>
-      <FreezerFlavor 
+      <FreezerFlavor
         key={flavorName}
         flavorName={flavorName}
-        onClickRestock={() => this.handleRestock(flavorName)}
+        onClickRestock={() => this.props.onClickRestock(flavorName)}
         scoops={flavors[flavorName]} />
     )
     return (
-      <Panel title={`Freezer (º${this.state.temperature}C)`}>
-        <Button label='Add flavor' onClick={this.handleClickAddFlavor} />
+      <Panel title={`Freezer (º${this.props.temperature}C)`}>
+        <Button label='Add flavor' onClick={this.props.onClickAddFlavor} />
         <br />
         {freezerFlavors}
       </Panel>
     );
-
   }
 }
 
 export default Freezer;
+
+
+  // import { actions as actionsFreezer } from "../../ducks/freezer";
+  // import store from "../../store";
+  // import * as FLAVORS from "../../constants/icecreams_flavors";
+
+
+  // state = {
+  //   flavors: store.getState().freezer.icecreams,
+  //   temperature: store.getState().freezer.temperature
+  // }
+
+  // componentDidMount() {
+  //   this.unsuscribe = store.subscribe(() =>
+  //     this.setState({
+  //       flavors: store.getState().freezer.icecreams,
+  //       temperature: store.getState().freezer.temperature
+  //     }))
+  //   setInterval(() => {
+  //     const randomTemp = -Math.round(Math.random() * 10)
+  //     store.dispatch(actionsFreezer.updateTemperature(randomTemp))
+  //   }, 2000)
+  // }
+
+  //en este caso no seria necesario por que siempre esta este comopnente en pantalla
+  // componentWillUnmount() {
+  //   this.unsuscribe()
+  // }
+
+  // handleRestock = (flavorName) => {
+  //   const amount = parseInt(window.prompt(`Enter number of scoops to restock ${flavorName}`))
+  //   if (!isNaN(amount)){
+  //     store.dispatch(actionsFreezer.addIcecream(flavorName, amount))
+  //   }
+  // }
+
+  // handleClickAddFlavor = () => {
+  //   const availableFlavors=Object.keys(FLAVORS)
+  //   const flavorToRestock =
+  //     window.prompt(`Enter flavor to restock from ${availableFlavors.join(',')}`)
+  //     if (FLAVORS[flavorToRestock]){
+  //       this.handleRestock(flavorToRestock)
+  //     }
+  // }
 
---
+++ src/components/Freezer/FreezerContainer.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import store from "../../store";
+import { actions as actionsFreezer } from "../../ducks/freezer";
+import * as FLAVORS from "../../constants/icecreams_flavors";
+import Freezer from "../Freezer/Freezer";
+// import Panel from '../Panel/Panel';
+// import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
+// import Button from "../Button/Button";
+
+
+class FreezerContainer extends Component {
+  state = {
+    flavors: store.getState().freezer.icecreams,
+    temperature: store.getState().freezer.temperature
+  }
+  componentDidMount() {
+    this.unsuscribe = store.subscribe(() =>
+      this.setState({
+        flavors: store.getState().freezer.icecreams,
+        temperature: store.getState().freezer.temperature
+      }))
+    setInterval(() => {
+      const randomTemp = -Math.round(Math.random() * 10)
+      store.dispatch(actionsFreezer.updateTemperature(randomTemp))
+    }, 2000)
+  }
+
+
+  //en este caso no seria necesario por que siempre esta este comopnente en pantalla
+  componentWillUnmount() {
+    this.unsuscribe()
+  }
+
+  handleRestock = (flavorName) => {
+    const amount = parseInt(window.prompt(`Enter number of scoops to restock ${flavorName}`))
+    if (!isNaN(amount)) {
+      store.dispatch(actionsFreezer.addIcecream(flavorName, amount))
+    }
+  }
+
+  handleClickAddFlavor = () => {
+    const availableFlavors = Object.keys(FLAVORS)
+    const flavorToRestock =
+      window.prompt(`Enter flavor to restock from ${availableFlavors.join(',')}`)
+    if (FLAVORS[flavorToRestock]) {
+      this.handleRestock(flavorToRestock)
+    }
+  }
+
+  render() {
+    // console.log(this.state);
+    // const flavors = this.state.flavors;
+    // const freezerFlavors = Object.keys(flavors).map(flavorName =>
+    //   <FreezerFlavor
+    //     key={flavorName}
+    //     flavorName={flavorName}
+    //     onClickRestock={() => this.handleRestock(flavorName)}
+    //     scoops={flavors[flavorName]} />
+    // )
+    return (
+      // <Panel title={`Freezer (º${this.state.temperature}C)`}>
+      //   <Button label='Add flavor' onClick={this.handleClickAddFlavor} />
+      //   <br />
+      //   {freezerFlavors}
+      // </Panel>
+      <Freezer
+        flavors={this.state.flavors}
+        temperature={this.state.temperature}
+        onClickAddFlavor={this.handleClickAddFlavor}
+        onClickRestock={this.handleRestock}
+      />
+    );
+
+  }
+}
+
+export default FreezerContainer;
+
